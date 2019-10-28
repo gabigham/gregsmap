94bfa98d27aea8c46f3cb5b56fb6d780fa18993f
George Bigham <gabigham4@gmail.com>
2019-10-28 16:51:50 -0700

    latest

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,35 +18,31 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
 
-/*
+
 var link = "static/data/stuff.geojson";
 
 // Grab the data with d3
 d3.json(link, function(response) {
-
+  geodata = response.features
+ // var features = [];
+  console.log(geodata)
+ // features = geoObject.features;
+  
+  console.log(geodata[0].geometry)
   // Create a new marker cluster group
-  var markers = L.markerClusterGroup();
+ // console.log(  features)
 
-  // Loop through data
-  for (var i = 0; i < response.length; i++) {
-
-    // Set the data location property to a variable
-    var location = response[i].location;
-
-    // Check for location property
-    if (location) {
-
-      // Add a new marker to the cluster group and bind a pop-up
-      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-        .bindPopup(response[i].descriptor));
-    }
-
+  // Loop through the geodata array and create one marker for each item, 
+  // bind a popup containing its title and age add it to the map
+  
+  for (var i = 0; i < geodata.length; i++) {
+    var thing = geodata[i];
+    L.marker(thing.geometry.coordinates)
+      .bindPopup("<h1>" + thing.title + "</h1> <hr> <h3>Population " + thing.age + "</h3>")
+      .addTo(myMap);
   }
-
-  // Add our marker cluster layer to the map
-  myMap.addLayer(markers);
-
+  
 });
 
 
-*/
+
